7fc79faac37fc0b8a32b60cdaf16838f59de82e9
Леонов Павел <angelos.lpp@yandex.ru>
2020-10-18 10:16:55 +0300

    Add comments

--- AnnGun.py
+++ AnnGun.py
@@ -1,18 +1,28 @@
 import math
+# Импортируем файл boot.py
 import boot
 from time import sleep
+# Импортируем файл calculate.py
 import calculate
 import datetime as dt
+# Импортируем библеотеки операционной системы
 import os
+
+# Устанавливаем силу свободного падения
 G = 9.81
+# Скорость заряда в м/с
 SPEED_B = 1000
+# Часть консоли
 CONSOLE_T = 'G.U.N.D.A.M. /># '
+# Имя файла куда будут записыватся данные
 DATA_R = 'datarecord.txt'
 
 
 class Gun:
     def __init__(self, distance):
+        # Проверка хочет ли оператор прочитать файл
         if distance == 1:
+            # Проверяем существует ли файл
             if os.path.exists(DATA_R):
                 with open(DATA_R, 'r', encoding='utf-8') as fileopen:
                     print(fileopen.read())
@@ -23,6 +33,7 @@ class Gun:
                 exit(0)
         else:
             self.distance = distance
+
     def shoot(self, rec):
         self.rec = rec
         return self.rec
@@ -38,38 +49,45 @@ class MotherBoard(Gun):
             pass
 
     def angle_var(self):
-            if self.distance <= 2000:
-                angle_g = (self.distance / (SPEED_B ** 2/G))/2
-                time_d = 2 * SPEED_B * math.sin(round(math.degrees(math.pi/2 - angle_g), 2))/G
-                calculate.calculate_main()
-                print(f"{boot.Fore.RED}Расстояние до цели: "
-                      f"{boot.Fore.GREEN}{self.distance} {boot.Fore.RED}"
-                      f"метров\n"
-                      f"{boot.Fore.RED}Необходимый угол: "
-                      f"{boot.Fore.GREEN}"
-                      f"{round(math.degrees(math.pi/2 - angle_g), 2)}"
-                      f" {boot.Fore.RED}"
-                      f"градусов\n"
-                      f"Необходимое время до цели: {boot.Fore.GREEN}{round(time_d, 2)}"
-                      f" {boot.Fore.RED}"
-                      f" секунд"
-                      f"{boot.Fore.RESET}")
-                self.shoot_gui(time_d)
-                print("Запись данных в файл...")
-                with open(DATA_R, 'a', encoding='utf-8') as fileopen:
-                    fileopen.write(f"Время на текущий момент: {dt.datetime.now()}\n"
-                                   f"Расстояние до цели: {self.distance} "
-                                   f"метров\n"
-                                   f"Угол наведения: {round(math.degrees(math.pi/2 - angle_g), 2)}"
-                                   f" градусов\n"
-                                   f"Время до цели: {round(time_d, 2)} секунд\n")
-                    fileopen.close()
-                return f"CAAABOOOMMMM"
-            else:
-                return f"Превышена максимальная дистанция стрельбы..."
+        # Проверяет, что бы входное значение для расстояния до цели
+        # было меньше или равно 2000 метров
+        if self.distance <= 2000:
+            angle_g = (self.distance / (SPEED_B ** 2 / G)) / 2
+            time_d = 2 * SPEED_B * math.sin(round(math.degrees(math.pi / 2 - angle_g), 2)) / G
+            # Вызов интерфейса
+            calculate.calculate_main()
+            print(f"{boot.Fore.RED}Расстояние до цели: "
+                  f"{boot.Fore.GREEN}{self.distance} {boot.Fore.RED}"
+                  f"метров\n"
+                  f"{boot.Fore.RED}Необходимый угол: "
+                  f"{boot.Fore.GREEN}"
+                  f"{round(math.degrees(math.pi / 2 - angle_g), 2)}"
+                  f" {boot.Fore.RED}"
+                  f"градусов\n"
+                  f"Необходимое время до цели: {boot.Fore.GREEN}{round(time_d, 2)}"
+                  f" {boot.Fore.RED}"
+                  f" секунд"
+                  f"{boot.Fore.RESET}")
+            self.shoot_gui(time_d)
+            print("Запись данных в файл...")
+            # Открываем файл в формате добавление данных с кодировкой
+            # UTF-8
+            with open(DATA_R, 'a', encoding='utf-8') as fileopen:
+                fileopen.write(f"Время на текущий момент: {dt.datetime.now()}\n"
+                               f"Расстояние до цели: {self.distance} "
+                               f"метров\n"
+                               f"Угол наведения: {round(math.degrees(math.pi / 2 - angle_g), 2)}"
+                               f" градусов\n"
+                               f"Время до цели: {round(time_d, 2)} секунд\n")
+                fileopen.close()
+            return f"CAAABOOOMMMM"
+        else:
+            return f"Превышена максимальная дистанция стрельбы..."
 
 
+# Запускаем эмуляцию загрузки операционной системы
 boot.boot()
+# Вводим данные
 c1 = MotherBoard(int(input(f"{CONSOLE_T}Укажите расстояние "
-                       f"до цели(метры) или наберите 1 для просмотра записей:\n>>> ")))
+                           f"до цели(метры) или наберите 1 для просмотра записей:\n>>> ")))
 print(c1.shoot(c1.angle_var()))
--- boot.py
+++ boot.py
@@ -1,9 +1,13 @@
 import os
 import time
+# Подключаем библиотеку tqdm для вывода прогресс бара
 from tqdm import tqdm
+# Подключаем библиотеку Fore (для изменения цвета)
+# и Style (для изменения стиля)
 from colorama import Fore
 from colorama import Style
 
+# Словарь для загрузки и времени в sec
 boot_name = {'loading': ['Начинается загрузка системы...', 3],
              'access_to_data': ['Проверка данных...', 6],
              'network_access': ['Проверка сетевого взаимодействия...', 2],
@@ -13,22 +17,32 @@ boot_name = {'loading': ['Начинается загрузка системы..
 
 
 def progress_bar(delta):
+    # Вызываем цикл для tqdm
     for i in tqdm(range(delta), ascii=True, colour='MAGENTA'):
+        # Спим 1 секунду
         time.sleep(1)
+        # Так как данных нам выводить не надо, то pass
         pass
+    # В конце печатаем [ OK ]
     print(f"{Fore.GREEN}[ OK ]")
 
 
 def boot():
+    # Шагаем по словарю
     for i in boot_name:
+        # Если i не равно last_message, то
         if i != 'last_message':
+            # Печатаем нулевой индекс
             print(f"{Fore.LIGHTRED_EX}{boot_name[i][0]}")
             dl = boot_name[i][1]
+            # Вызываем прогресс бар
             progress_bar(dl)
+    # Выводим последнее сообщение
     print(f"{Style.RESET_ALL}{Fore.RESET}"
           f"{boot_name['last_message'][0]}. \nЦитаты великих людей:\n"
           f"{Style.BRIGHT}"
           f"Обязательно бахнем! И не раз! Весь мир в труху!… Но потом."
           f"{Fore.RESET}{Style.RESET_ALL}")
     time.sleep(3)
+    # Очистка консольного окна
     os.system('cls' if os.name == 'nt' else 'clear')
